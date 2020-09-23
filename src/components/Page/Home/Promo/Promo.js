00d1294943be6507953c104e4e29b9fa89c644e7
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-23 19:23:02 +0300

    Promo slider logic and styles

--- src/components/Page/Home/Promo/Promo.js
+++ src/components/Page/Home/Promo/Promo.js
@@ -1,44 +1,46 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 import { useImmer } from "use-immer";
 import promo1 from "../../../../images/promo1.jpg";
 import promo2 from "../../../../images/promo2.jpg";
 import promo3 from "../../../../images/promo3.jpg";
 
 export default function Promo(props) {
-  const [sliderStep, setSliderStep] = useImmer(0);
-  const sliderImages = [promo1, promo2, promo3];
-  const sliderText = ["Promo text 1", "Promo text 2", "Promo text 3"];
+  const [slider, setSlider] = useImmer({
+    step: 0,
+    images: [promo1, promo2, promo3],
+    text: ["Promo text 1", "Promo text 2", "Promo text 3"],
+  });
 
-  console.log(sliderStep);
+  let timeout = useRef();
 
   useEffect(() => {
-    setInterval(() => {
-      if (sliderStep == 2) {
-        changeStep(0);
-      } else {
-        changeStep(sliderStep + 1);
-      }
-    }, 1000);
-  }, []);
+    timeout.current = setTimeout(() => {
+      changeStep((slider.step + 1) % 3);
+    }, 2000);
+  }, [slider.step]);
 
   function changeStep(n) {
-    setSliderStep((draft) => (draft = n));
+    clearTimeout(timeout.current);
+    setSlider((draft) => {
+      draft.step = n;
+    });
   }
 
   return (
     <section className="promo">
-      <img src={sliderImages[sliderStep]} alt="promo" />
-      <h2>{sliderText[sliderStep]}</h2>
+      <div className="promo__slide">
+        <img src={slider.images[slider.step]} alt="promo" />
+        <h2 className="promo__title">{slider.text[slider.step]}</h2>
+      </div>
       <div className="promo__nav">
-        <div className="promo__slider-step" onClick={() => changeStep(0)}>
-          <div></div>
-        </div>
-        <div className="promo__slider-step" onClick={() => changeStep(1)}>
-          <div></div>
-        </div>
-        <div className="promo__slider-step" onClick={() => changeStep(2)}>
-          <div></div>
-        </div>
+        {slider.images.map((image, index) => (
+          <div
+            className={`promo__slider-step ${index === slider.step ? "promo__slider-step--active" : ""}`}
+            onClick={() => changeStep(index)}
+          >
+            <div></div>
+          </div>
+        ))}
       </div>
     </section>
   );
