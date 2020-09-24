f754419a46197f29fa4957e01554a26f9aabfec9
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-24 15:32:18 +0300

    className bugfix

--- src/components/Page/Home/CategoryItems/CategoryItems.js
+++ src/components/Page/Home/CategoryItems/CategoryItems.js
@@ -2,12 +2,12 @@ import React from "react";
 
 export default function CategoryItems({ data: { title, products } }) {
   return (
-    <div class="category-items__category">
+    <div className="category-items__category">
       <h2 className="category-items__title">{title}</h2>
       <div className="category-items__list-of-items">
         {products.slice(0, 4).map((product) => (
           <div className="category-items__product" key={product.id}>
-            <div class="category-items__image-holder">
+            <div className="category-items__image-holder">
               <img src={`http://smktesting.herokuapp.com/static/${product.img}`} alt={product.img} />
             </div>
             <h3>{product.title}</h3>
