833de6decfc3d8f4b8fa255ee92f6f3edd041b08
Alex-Rudoy <rudoy4ik@gmail.com>
2020-09-24 14:45:24 +0300

    AuthModal component

--- src/reducer/Reducer.js
+++ src/reducer/Reducer.js
@@ -1,19 +1,20 @@
 export function Reducer(draft, action) {
   switch (action.type) {
     case "showLoginModal":
-      draft.isLoginModalOpen = true;
-      draft.isRegisterModalOpen = false;
+      draft.isAuthModalOpen = true;
+      draft.modalTab = "login";
       return;
     case "showRegisterModal":
-      draft.isRegisterModalOpen = true;
-      draft.isLoginModalOpen = false;
+      draft.isAuthModalOpen = true;
+      draft.modalTab = "register";
+      return;
+    case "closeAuthModal":
+      draft.isAuthModalOpen = false;
       return;
     case "toggleSettingsPopup":
       draft.isSettingsPopupOpen = !draft.isSettingsPopupOpen;
       return;
-    case "closeAllModals":
-      draft.isRegisterModalOpen = false;
-      draft.isLoginModalOpen = false;
+    case "closeSettingsPopup":
       draft.isSettingsPopupOpen = false;
       return;
     case "logout":
